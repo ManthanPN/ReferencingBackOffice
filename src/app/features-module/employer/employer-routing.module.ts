e86d4ae6d1de41d8f69d8cce131e074cf663637d
Manthan Prajapati <manthan.prajapati@pib-insurance.com>
2024-04-29 15:00:59 +0530

    Commit (employer)

--- src/app/features-module/employer/employer-routing.module.ts
+++ src/app/features-module/employer/employer-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployerDetailsComponent } from './employer-details/employer-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: EmployerDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features-module/employer/employer.module.ts
+++ src/app/features-module/employer/employer.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployerRoutingModule } from './employer-routing.module';
+import { EmployerDetailsComponent } from './employer-details/employer-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployerDetailsComponent
+  ],
   imports: [
     CommonModule,
     EmployerRoutingModule

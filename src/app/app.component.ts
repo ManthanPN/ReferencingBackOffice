cdcb939be4897866ffd661b35a8e89e769453ac4
mohit.kumar@pib-insurance.com <mohit.kumar@pib-insurance.com>
2024-04-29 14:53:11 +0530

    Kendo UI fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { DashboardComponent } from './features-module/application/dashboard/dash
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ReferencingBackOffice';
--- src/app/features-module/application/dashboard/dashboard.component.ts
+++ src/app/features-module/application/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import {
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   myForm = new FormGroup({});
@@ -16,40 +16,46 @@ export class DashboardComponent {
   options: FormlyFormOptions = {};
   fields: FormlyFieldConfig[];
 
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {
     this.setForm();
   }
-  onSubmit(){}
+  onSubmit() {}
   setForm() {
     this.fields = [
       {
         fieldGroupClassName: 'row my-2',
         fieldGroup: [
           {
-            className: 'col-12',
-            key: 'schoolId',
+            className: 'col-sm-3',
+            type: 'input',
+            key: 'applicantId',
+            props: {
+              label: 'Applicant ID',
+            },
+          },
+          {
+            template: `<button style:"display:flex;text-align:center;justify-content:center">Search</button>`,
+            className: 'col-sm-1 btn btn-outline-primary btn-sm',
+          },
+          {
+            className: 'col-sm-2',
+            key: 'status',
             type: 'select',
             props: {
-              label: 'School',
-              options:  [
-                        { value: 1, label: 'Option 1' },
-                        { value: 2, label: 'Option 2' },
-                        { value: 3, label: 'Option 3' },
-                      ],
+              label: 'Status',
+              options: [
+                { value: 1, text: 'Completed' },
+                { value: 2, text: 'Ongoing' },
+                { value: 3, text: 'Planning' },
+              ],
               primitive: true,
               required: true,
             },
           },
-        ],
-      },
-     
-      {
-        fieldGroupClassName: 'row my-2',
-        fieldGroup: [
           {
-            className: 'col-12',
+            className: 'col-sm-3',
             key: 'startDate',
             type: 'date',
             props: {
@@ -59,9 +65,91 @@ export class DashboardComponent {
               format: 'dd/MM/yyyy',
             },
           },
+          {
+            className: 'col-sm-3',
+            key: 'endDate',
+            type: 'date',
+            props: {
+              label: 'End Date',
+              primitive: true,
+              required: true,
+              format: 'dd/MM/yyyy',
+            },
+          },
         ],
       },
-     
+      {
+        fieldGroupClassName: 'row',
+        fieldGroup: [
+          {
+            className: 'col-sm-4',
+            type: 'input',
+            key: 'globalSearch',
+            props: {
+              label: 'Global Search',
+            },
+          },
+          {
+            template: `<button style:"display:flex;text-align:center;justify-content:center">Search</button>`,
+            className: 'col-sm-1 btn btn-outline-primary btn-sm',
+          },
+          {
+            className: 'col-sm-3',
+            key: 'agentType',
+            type: 'select',
+            props: {
+              label: 'Agent Type',
+              options: [
+                { value: 1, text: 'Type 1' },
+                { value: 2, text: 'Type 2' },
+              ],
+              primitive: true,
+              required: true,
+            },
+          },
+          {
+            className: 'col-sm-4',
+            type: 'input',
+            key: 'agentName',
+            props: {
+              label: 'Agent Name',
+            },
+          },
+        ],
+      },
+      {
+        fieldGroupClassName:'row',
+        fieldGroup:[
+          {
+            className: 'col-sm-3',
+            key: 'highRiskTaskId',
+            type: 'select',
+            props: {
+              label: 'High risk task',
+              options: [
+                { value: 1, text: 'Yes' },
+                { value: 2, text: 'No' },
+              ],
+              primitive: true,
+              required: true,
+            },
+          },
+          {
+            className: 'col-sm-3',
+            key: 'validationTask',
+            type: 'select',
+            props: {
+              label: 'Validation task',
+              options: [
+                { value: 1, text: 'Yes' },
+                { value: 2, text: 'No' },
+              ],
+              primitive: true,
+              required: true,
+            },
+          },
+        ]
+      }
     ];
   }
 }
